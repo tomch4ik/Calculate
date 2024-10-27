db98a855651ba9eed3cd737b82ba2ae97be06f8d
Samof_qs56@student.itstep.org <Samof_qs56@student.itstep.org>
2024-10-27 10:37:33 +0200

    passport

---
+++ Calculate/Passport.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate
+{
+    internal class Passport
+    {
+        public int PassportNumber { get; private set; }
+        public string FullName { get; private set; }
+        public DateTime DateStart { get; private set; }
+        public DateTime DateEnd { get; private set; }
+        public string Nationality { get; private set; }
+
+        public Passport(int passportNumber, string fullName, DateTime datestart, DateTime dateend, string nationality)
+        {
+            PassportNumber = passportNumber; 
+            FullName = ValidateFullName(fullName);
+            DateStart = datestart;
+            DateEnd = ValidateEndDate(datestart, dateend);
+            Nationality = ValidateNationality(nationality);
+        }
+        private string ValidateFullName(string fullName)
+        {
+            if (string.IsNullOrWhiteSpace(fullName))
+                throw new ArgumentException("Поле ввода обязательно должно быть заполнено");
+            return fullName;
+        }
+        private DateTime ValidateEndDate(DateTime datestart, DateTime dateend)
+        {
+            if (dateend <= datestart)
+                throw new ArgumentException("Дата окончания действия заграничного паспорта не может быть раньше даты выдачи");
+            return dateend;
+        }
+        private string ValidateNationality(string nationality)
+        {
+            if (string.IsNullOrWhiteSpace(nationality))
+                throw new ArgumentException("Поле ввода обязательно должно быть заполнено");
+            return nationality;
+        }
+        public void OutputPassportInfo()
+        {
+            Console.WriteLine("Заграничный паспорт:");
+            Console.WriteLine($"Номер паспорта: {PassportNumber}");
+            Console.WriteLine($"ФИО владельца: {FullName}");
+            Console.WriteLine($"Дата выдачи паспорта: {DateStart:dd.MM.yyyy}");
+            Console.WriteLine($"Дата окончания действия паспорта: {DateEnd:dd.MM.yyyy}");
+            Console.WriteLine($"Национальность: {Nationality}");
+        }
+    }
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Passport passport = new Passport(1234567890, "Малиновский Николай Иванович", new DateTime(2024, 10, 27), new DateTime(2029, 10, 27), "Украинец");
+                passport.OutputPassportInfo();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Calculate/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Calculate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите тип перевода:");
+            Console.WriteLine("1 - Из десятичной в двоичную");
+            Console.WriteLine("2 - Из десятичной в шестнадцатеричную");
+            Console.WriteLine("3 - Из двоичной в десятичную");
+            Console.WriteLine("4 - Из шестнадцатеричной в десятичную");
+            string choice = Console.ReadLine();
+            Console.Write("Введите число: ");
+            string input = Console.ReadLine();
+            try
+            {
+                switch (choice)
+                {
+                    case "1":
+                        int type1 = int.Parse(input);
+                        Console.WriteLine("Результат: " + Convert.ToString(type1, 2));
+                        break;
+
+                    case "2":
+                        int type2 = int.Parse(input);
+                        Console.WriteLine("Результат: " + Convert.ToString(type2, 16));
+                        break;
+
+                    case "3":
+                        int type3 = Convert.ToInt32(input, 2);
+                        Console.WriteLine("Результат: " + type3);
+                        break;
+
+                    case "4":
+                        int type4 = Convert.ToInt32(input, 16);
+                        Console.WriteLine("Результат: " + type4);
+                        break;
+
+                    default:
+                        Console.WriteLine("Неправильный выбор. Попробуйте снова.");
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: неверный формат числа.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Ошибка: число выходит за пределы допустимого диапазона.");
+            }        
+        }
+    }
+}
